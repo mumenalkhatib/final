eb7d60065abd3ca4cffeda9b54c2390c45c92c1b
Mumen alkhatib <mumen_alkhatib@yahoo.com>
2017-07-27 10:53:32 +0300

    finaly we made a website222

--- app.py
+++ app.py
@@ -16,25 +16,24 @@ def reset_db():
 @app.route('/register' ,methods=["GET" , "POST"])
 def registerpage():
 	registerTable = db["register"]
-	allr = list(registerTable.all())[::-1]
+	allr = list(registerTable.all())
 	if request.method == 'GET':
 		return render_template('register.html')
 	else :
 		form =request.form
 		n_email=form["n_email"]
-		password=form["password"]
 		f_name =form["f_name"]
 		l_name=form["l_name"]
 		us_name=form["us_name"]
 		w_site=form["w_site"]
 
-		entry = {"n_email":n_email ,"password":password , "f_name":f_name , "l_name":l_name , "us_name":us_name , "w_site":w_site }
+		entry = {"n_email":n_email , "f_name":f_name , "l_name":l_name , "us_name":us_name , "w_site":w_site }
 
 		NameToCheck = us_name
 		print len(list(registerTable.find(us_name=NameToCheck)))
 		if len(list(registerTable.find(us_name=NameToCheck))) == 0:
 			registerTable.insert(entry)
-			allr = list(registerTable.all())[::-1]
+			allr = list(registerTable.all())
 			return redirect('/feed')
 		else:
 			return redirect('/error')
@@ -69,7 +68,7 @@ def feedpage():
 @app.route('/list')
 def listpage():
 	registerTable = db["register"]
-	allcontacts = list (registerTable.all())
+	allcontacts = list (registerTable.all())[::-1]
 	return render_template ("list.html" , contacts = allcontacts)
 #..............................................#
 
